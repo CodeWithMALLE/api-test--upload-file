dfe563f442d8c36226ddb0245ea923ef53816246
moussasamina <moussa.malle@samina.tech>
2024-10-16 11:45:00 +0000

    api-demo-upload-file

---
+++ src/app/api/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import fs from "fs"
+import path from "path";
+
+export async function POST(request) {
+    const data = await request.formData();
+    const file = data.get("file")
+
+    const fileName = file.name;
+    const filePath = path.join(process.cwd(), "src", fileName);
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    await fs.promises.writeFile(filePath, buffer);
+
+    const fileUrl = `/uploads/${fileName}`;
+
+    return NextResponse.json({ fileUrl });
+    
+}
